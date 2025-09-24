262ff71a7d77f5bd97d97b027d2f2bdf2a1db134
Tamilmani328 <muthukavitha111@gmail.com>
2025-09-24 21:32:28 +0530

    day 3
    using html and java script

---
+++ exercise problem/day3(20.09.25)/script.js
@@ -0,0 +1,54 @@
+//CODING CHALLENGE 5
+
+// 1. Arrow function to calculate average
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// 2. Test data
+// DATA 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+// 3. Function to check winner
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win`);
+  } else {
+    console.log('No team wins');
+  }
+}
+
+// 4. Check winner for DATA 1
+checkWinner(scoreDolphins, scoreKoalas);
+
+// DATA 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+
+// Check winner for DATA 2
+checkWinner(scoreDolphins, scoreKoalas);
+
+// .....................................................
+// CODING CHALLENGE 6
+
+const calcTip = function(bill)
+{
+    return bill >=50 && bill <=300 ? bill * 0.15:bill*20;
+};
+
+const bills=[125,555,44];
+
+const tips=[];
+const total=[];
+
+for(let i=0;i<bills.length;i++)
+{
+    const tip=calcTip(bills[i]);
+    tips.push(tip);
+    total.push(bills[i]+tip);
+
+}
+console.log("Bills:",bills);
+console.log("Tips:",tips);
+console.log("Total:",total);
